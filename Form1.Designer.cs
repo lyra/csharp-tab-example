58561e8c7ff68aa5ee17e031a071d662fcc79d11
Clement Brunie <clement.brunie@lyra-network.com>
2021-07-21 11:40:15 +0200

    feat: csharp tab example initialization

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace csharp_tab_example
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KJS = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.KJS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // KJS
+            // 
+            this.KJS.CreationProperties = null;
+            this.KJS.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.KJS.Location = new System.Drawing.Point(12, 101);
+            this.KJS.Name = "KJS";
+            this.KJS.Size = new System.Drawing.Size(776, 337);
+            this.KJS.Source = new System.Uri("https://edge.purebilling.io/demo/minimalEmbeddedForm.test.php?theme=classic", System.UriKind.Absolute);
+            this.KJS.TabIndex = 0;
+            this.KJS.ZoomFactor = 1D;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Name";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 23);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Mail";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.KJS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.KJS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 KJS;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
